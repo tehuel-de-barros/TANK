6d3eda05e3f602d82e07ee5506b091b39fcc1c51
tehuel-de-barros <tehueldebarros@gmail.com>
2025-02-22 03:27:59 -0300

    niveles, win

--- core/src/main/java/com/vic/blocktanks/utilidades/Recurso.java
+++ core/src/main/java/com/vic/blocktanks/utilidades/Recurso.java
@@ -1,8 +1,9 @@
 package com.vic.blocktanks.utilidades;
 
 public class Recurso {
-    public static final String CARGA = "fondos/pantallaCarga.jpg";
-    public static final String FONDOMENU = "fondos/fondoMenu.jpg";
+    public static final String CARGA      = "fondos/pantallaCarga.jpg";
+    public static final String FONDOMENU  = "fondos/fondoMenu.jpg";
+    public static final String GAMEOVER   = "fondos/gameOver.jpeg";
+    public static final String WIN        = "fondos/win.jpeg";
     public static final String FUENTEMENU = "fuentes/OSWALD.otf";
-    public static final String GAMEOVER = "fondos/gameOver.jpg";  // Nueva constante para Game Over
 }
--- lwjgl3/src/main/java/com/vic/blocktanks/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/vic/blocktanks/lwjgl3/Lwjgl3Launcher.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.vic.blocktanks.BlockTanks;
 import com.vic.blocktanks.utilidades.Config;
 
-/** Launches the desktop (LWJGL3) application. */
+/** Launches the desktop (LWJGL3) application. */ 
 public class Lwjgl3Launcher {
     public static void main(String[] args) {
         if (StartupHelper.startNewJvmIfRequired()) return; // This handles macOS support and helps on Windows.
